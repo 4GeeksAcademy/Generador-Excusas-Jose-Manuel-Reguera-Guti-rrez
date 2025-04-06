0c710f4e36794209fd5ca83ecb12ac8d5d9d7171
zoscra <jmrg.trabajo@gmail.com>
2025-04-06 17:49:32 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
+
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector('#excuse').innerHTML = generadorExcusas();
+}
+
+let generadorExcusas =()=>{
+
+let quien = ['mi perro ','mi novia ','un rey mago ','el Papa '];
+let accion = ['se cago en mi  ','me robo mi ','se sento en mi ', 'lucho con mi'];
+let cosaRota = ['tarea ','ordenador ','oso ','vecino '];
+let donde = ['en la luna.','en mi bañera.','en el Vaticano', 'en un barco.']
+
+let indexQuien =  Math.floor(Math.random()*quien.length);   
+let indexAccion =  Math.floor(Math.random()*accion.length);
+let indexCosaRota =  Math.floor(Math.random()*cosaRota.length);
+let indexDonde =  Math.floor(Math.random()*donde.length);
+
+return quien[indexQuien]+accion[indexAccion]+cosaRota[indexCosaRota]+donde[indexDonde];
+
 };
